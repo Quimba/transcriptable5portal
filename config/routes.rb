9285f2c6534b43b1160bca5c3b99ab4b860cbfa3
Quimba <bjquimba@gmail.com>
2014-11-18 00:50:58 +0000

    added styling to the invoice table

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
   get 'welcome/about'
-
   get 'welcome/contact'
-
   get 'welcome/faq'
-
   get 'welcome/pricing'
-
   get 'welcome/features'
 
   resources :invoices
